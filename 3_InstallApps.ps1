5060dee2b66d50e4f3d3d1dc538423571a7115fc
JamiebMSFT <65600400+JamiebMSFT@users.noreply.github.com>
2021-12-06 10:12:54 +0000

    AVD

---
+++ 3_InstallApps.ps1
@@ -0,0 +1,12 @@
+ 
+ # install Winget Packages
+ winget install Microsoft.SQLServerManagementStudio
+ winget install Microsoft.PowerBI
+ winget install JetBrains.PyCharm.Community
+ winget install Tableau.Desktop
+ winget install Notepad++.Notepad++
+ winget install Postman.Postman
+ winget install 7zip.7zip.Alpha.msi
+ winget install Microsoft.VisualStudio.2019.Professional
+ winget install RStudio.RStudio.OpenSource
+ 

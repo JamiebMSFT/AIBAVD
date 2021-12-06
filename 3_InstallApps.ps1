a1aeab6efd13ba38fe5db9cb656cf2a3ef468477
JamiebMSFT <65600400+JamiebMSFT@users.noreply.github.com>
2021-12-06 11:15:19 +0000

    Update 3_InstallApps.ps1

--- 3_InstallApps.ps1
+++ 3_InstallApps.ps1
@@ -1,13 +1,12 @@
  
  # install Winget Packages
- winget install Microsoft.SQLServerManagementStudio
- winget install Microsoft.PowerBI
- winget install JetBrains.PyCharm.Community
- winget install Tableau.Desktop
- winget install Notepad++.Notepad++
- winget install Postman.Postman
- winget install 7zip.7zip.Alpha.msi
- winget install Microsoft.VisualStudio.2019.Professional
- winget install RStudio.RStudio.OpenSource
- Write-Host "Exit code: " $LASTEXITCODE
+ winget install Microsoft.SQLServerManagementStudio --silent
+ winget install Microsoft.PowerBI -silent
+ winget install JetBrains.PyCharm.Community -silent
+ winget install Tableau.Desktop -silent
+ winget install Notepad++.Notepad++ -silent
+ winget install Postman.Postman -silent
+ winget install 7zip.7zip.Alpha.msi -silent
+ winget install Microsoft.VisualStudio.2019.Professional -silent
+ winget install RStudio.RStudio.OpenSource -silent
  write-host 'AIB Customization: Finished App Installs'
